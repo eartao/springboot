9394a77396119f9516b6e3d5361cf92aec4f1deb
eartao <861811152@qq.com>
2018-08-29 23:59:24 +0800

    push

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,18 +1,46 @@
+import org.apache.catalina.filters.WebdavFixFilter;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.*;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
 
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 public class SeleniumTest {
-    public static void main(String[] args) throws Exception{
-        System.setProperty("webdriver.gecko.driver","D:\\Driver\\geckodriver.exe");
-        WebDriver driver = new FirefoxDriver();
-        driver.manage().deleteAllCookies();
+    public static void main(String[] args) throws Exception {
+//设置必要参数
+        DesiredCapabilities dcaps = new DesiredCapabilities();
+        //ssl证书支持
+        dcaps.setCapability("acceptSslCerts", true);
+        //截屏支持
+        dcaps.setCapability("takesScreenshot", true);
+        //css搜索支持
+        dcaps.setCapability("cssSelectorsEnabled", true);
+        //js支持
+        dcaps.setJavascriptEnabled(true);
+        dcaps.setCapability("phantomjs.binary.path","D:\\Driver\\phantomjs.exe");
+        PhantomJSDriver driver = new PhantomJSDriver(dcaps);
+
+        //设置隐性等待（作用于全局）
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        //打开页面
         driver.get("https://www.baidu.com/");
-        driver.findElement(By.id("kw")).sendKeys("java");
-        TimeUnit.SECONDS.sleep(5);
-        driver.findElement(By.id("su")).click();
+        WebDriverWait webDriverWait = new WebDriverWait(driver,10);
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.id("su")));
+        System.out.println(driver.getPageSource());
+        //查找元素
+        WebElement element = driver.findElement(By.id("su"));
+        System.out.println(element.getTagName());
+        driver.quit();
     }
+
 
 }
